a1a4b3eb3611d140b4a2d1976f20ddbf6e5f688c
ifan3735 <ifanmaina@gmail.com>
2024-07-23 14:11:33 +0300

    38 commit

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -45,8 +45,8 @@ export const createCheckoutSession = async (amount: number, currency: string, bo
             },
         ],
         mode: 'payment',
-        success_url: 'http://localhost:5173/bookings',
-        cancel_url: 'http://localhost:5173/bookings',
+        success_url: 'https://brave-forest-03edfb90f.5.azurestaticapps.net',
+        cancel_url: 'https://brave-forest-03edfb90f.5.azurestaticapps.net',
     });
 
     const amountAsString = amount.toString();
